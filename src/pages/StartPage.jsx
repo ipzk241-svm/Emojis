997caf89b314d4450d43b7bd214cf79fdb57808a
SergiikoVasya <sergiikovasya@gmail.com>
2025-10-03 14:59:40 +0300

    feat: added start Game on start page but no routing

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 import Settings from "../components/settings/Settings";
 import "../styles/App.css";
 import ModalWindow from "../components/ui/ModalWindow";
+import useGameLogic from "../hooks/UseGameLogic";
 
 const StartPage = () => {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [startGame] = useGameLogic();
 
+  const handleStartGame = () => {
+    // move to GamePage
+    startGame();
+  };
+aa
   return (
     <div className="start-page">
-      <button className="button">Почати гру</button>
+      <button className="button" onClick={handleStartGame}>
+        Почати гру
+      </button>
 
       <button className="button" onClick={() => setIsSettingsOpen(true)}>
         Налаштування
